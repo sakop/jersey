d53886dc529b575ba425457aebda95b2ccbcbd5b
qiucheng <sakopqiu@gmail.com>
2014-09-12 14:21:19 +0900

    todos resources

--- src/main/java/de/vogella/jersey/jaxb/model/Todo.java
+++ src/main/java/de/vogella/jersey/jaxb/model/Todo.java
@@ -3,11 +3,27 @@ package de.vogella.jersey.jaxb.model;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
-// JAX-RS supports an automatic mapping from JAXB annotated class to XML and JSON
-// Isn't that cool?
 public class Todo {
+    private String id;
     private String summary;
     private String description;
+
+    public Todo() {
+
+    }
+
+    public Todo(String id, String summary) {
+        this.id = id;
+        this.summary = summary;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getSummary() {
         return summary;
